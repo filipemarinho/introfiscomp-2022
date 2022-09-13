e91c71ff01ca4f89f6329401613672bda65c0463
Filipe Marinho <40862255+filipemarinho@users.noreply.github.com>
2022-09-12 21:42:53 -0300

    Add exemplos de input e output com read e write

---
+++ input_output.f90
@@ -0,0 +1,24 @@
+! compilar: gfortran input_output.f90 -Wall -Wextra -o input_output
+! rodar: ./input_output
+
+program readwrite
+   implicit none
+
+   integer :: N
+
+   print *, "Digite um número inteiro:\n" 
+   read*, N
+   print *, "Recebido: N", N
+		
+   !Cria um novo arquivo data.dat, dando o apelido 1
+   open(1,file = 'data.dat', status='new')
+
+   write(1,*) N
+   close(1) !não esqueça de fechar depois
+
+   !Cria outro arquivo com outro apelido	
+   open(2,file = 'data2N.dat', status='new')
+   write(2,*) N*2
+   close(2)
+
+end program readwrite
